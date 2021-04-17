e8bdbad4dfc935ffb0e1a26f4e3bd25a289b56dc
Ekeu Ulrich <ekeuulrich2@gmail.com>
2021-04-17 12:10:07 +0200

    Added Redux thunks to handle functions call and events asynchronously using Promise Pattern .i.e switched from google's onSnapShot to Redux-Thunk

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,56 @@
 import ShopActionTypes from './shop.types';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
 
-export const updateCollections = (collectionsMap) => ({
+/* export const updateCollections = (collectionsMap) => ({
   type: ShopActionTypes.UPDATE_COLLECTIONS,
   payload: collectionsMap,
+}); */
+
+/**
+ * Redux-Thunk Actions
+ *
+ * All thunks are is a action creator that returns a function
+ * that get's the dispatch (Similar to mapDispatchToProps)
+ *
+ * So instead of writing an action (the above updateCollections) that returns
+ * an action (action object), we are going to write a function that returns a
+ * function that get's dispatch in it so that whenever dispatch is called it will
+ * fire multiple actions
+ */
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap
+});
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+});
+
+/**
+ * Note ==> If redux-thunk middleware is enabled, any time you attempt to dispatch
+ * a function instead of an object, the middleware will call that function with 
+ * dispatch method itself as the first argument
+ * 
+ */
+//fetchCollectionsStartAsync ==> It's an async action creator
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart())
+
+    collectionRef
+    .get()
+    .then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      dispatch(fetchCollectionsSuccess(collectionsMap))
+    })
+    .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
--- src/redux/shop/shop.types.js
+++ src/redux/shop/shop.types.js
@@ -1,5 +1,20 @@
-const ShopActionTypes = {
+/**
+ * This is used or was used because we handled all the async activity
+ * in the shop component using the .get() or directly using firebase onSnapShot()
+ */
+/* const ShopActionTypes = {
     UPDATE_COLLECTIONS:'UPDATE_COLLECTIONS'
-}
+} */
 
-export default ShopActionTypes
+/**
+ * Using Redux-Thunk we are now going to set multiple states that our shop actions
+ * could be in as far as fetching the async data goes
+ */
+
+const ShopActionTypes = {
+  FETCH_COLLECTIONS_START: 'FETCH_COLLECTIONS_START',
+  FETCH_COLLECTIONS_SUCCESS: 'FETCH_COLLECTIONS_SUCCESS',
+  FETCH_COLLECTIONS_FAILURE: 'FETCH_COLLECTIONS_FAILURE',
+};
+
+export default ShopActionTypes;
